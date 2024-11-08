c3cbd81ed0460f8f8d329c38e014c8483d270c5c
이유진 <dldbwls0818@gmail.com>
2024-11-08 14:47:30 +0900

    rest study

--- 17_rest.js
+++ 17_rest.js
@@ -12,6 +12,11 @@ const skills = [
 ]
 
 const server = http.createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
     const url = req.url
     const method = req.method
     if (method === 'GET') {
